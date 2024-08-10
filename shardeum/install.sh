dc97c9f894f51c8fccb63a8eb9f41b866fbccec4
VadimRM7 <142230283+VadimRM7@users.noreply.github.com>
2024-08-10 09:59:56 +0200

    Update install.sh

--- shardeum/install.sh
+++ shardeum/install.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
 apt install expect -y
-. <(wget -qO- https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh) &>/dev/null
-. <(wget -qO- https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh) &>/dev/null
-wget -qO ./update_shard.sh https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
-chmod +x ./update_shard.sh
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
+curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh && chmod +x installer.sh
 
 expect -c '
-#!/usr/bin/expect
-
 set timeout 5
 
-spawn ./update_shard.sh
-
+spawn ./installer.sh
 while 1 {
-    interact {
-        -nobuffer -o "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?" {send "y\r"}
-        "What base directory should the node use (default ~/.shardeum):" {send "\r"}
-        "Do you want to run the web based Dashboard? (Y/n):" {send "\r"}
-        "Do you really want to upgrade now (y/N)?" {send "y\r"}
-        "Do you want to change the password for the Dashboard? (y/N):" {send "\r"}
-        "Enter the port (1025-65536) to access the web based Dashboard" {send "\r"}
-        "If you wish to set an explicit external IP, enter an IPv4 address" {send "\r"}
-        "If you wish to set an explicit internal IP, enter an IPv4 address" {send "\r"}
-        "Enter the first port (1025-65536) for p2p communication" {send "\r"}
-        "Enter the second port (1025-65536) for p2p communication" {send "\r"}
+
+    expect {
+        "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "y\r"; exp_continue}
+        "What base directory should the node use (default ~/.shardeum):" {send "\r"; exp_continue}
+        "Do you want to run the web based Dashboard? (Y/n):" {send "\r"; exp_continue}
+        "Set the password to access the Dashboard:" {send "111111\r"; exp_continue}
+        "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180\r"; exp_continue}
+        "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
+        "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
+        "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication (default 9001):" {send "\r"; exp_continue}
+        "Enter the second port (1025-65536) for p2p communication (default 10001):" {send "\r"; exp_continue}
         eof {break}
     }
-}'
+}
+'
 
-rm ./update_shard.sh
-cd $HOME
+rm ./installer.sh
 source $HOME/.shardeum/.env
+cd $HOME
+docker exec -i shardeum-dashboard operator-cli start
 
-echo -e "\033[1;31;40mShardeum обновлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
+echo -e "\033[1;31;40mShardeum установлен. Проверь количество токенов в explorer-sphinx.shardeum.org и делай стейк!\033[m"
