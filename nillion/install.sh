9be305d7cb92e94e7f9cc5e8dc31da97635cc6d5
VadimRM7 <142230283+VadimRM7@users.noreply.github.com>
2024-10-10 15:24:51 +0200

    Update install.sh

--- nillion/install.sh
+++ nillion/install.sh
@@ -8,6 +8,8 @@ sudo apt-get install --only-upgrade -y docker-ce docker-ce-cli containerd.io
 sudo systemctl restart docker
 docker --version
 
+docker rm $(docker ps -a -q --filter ancestor=nillion/verifier:v1.0.1)
+
 # Закидываем докер ниллион
 if docker pull nillion/verifier:v1.0.1; then
   echo "Образ успешно загружен"
@@ -18,5 +20,5 @@ fi
 
 mkdir -p nillion/verifier
 sleep 30
-docker run -v ./nillion/verifier:/var/tmp nillion/verifier:v1.0.1 initialise
+docker run --name run_nillion -v ./nillion/verifier:/var/tmp nillion/verifier:v1.0.1 initialise
 echo "Установка завершена!"
